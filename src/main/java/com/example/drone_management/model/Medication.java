055d3f89beaee0f367a35cf3588a02754c9a5835
Kudzai M <kudzaim@sonity.net>
2023-04-18 09:17:46 +0200

    Application.yaml Config

--- src/main/java/com/example/drone_management/model/Medication.java
+++ src/main/java/com/example/drone_management/model/Medication.java
@@ -3,10 +3,20 @@ package com.example.drone_management.model;
 import javax.swing.*;
 
 public class Medication {
+
+    private String medicationId;
     private String medicationName;
     private Double weight;
     private String code;
     private String image;
+
+    public String getMedicationId() {
+        return medicationId;
+    }
+
+    public void setMedicationId(String medicationId) {
+        this.medicationId = medicationId;
+    }
 
     public String getMedicationName() {
         return medicationName;
@@ -43,7 +53,8 @@ public class Medication {
     public Medication() {
     }
 
-    public Medication(String medicationName, Double weight, String code, String image) {
+    public Medication(String medicationId, String medicationName, Double weight, String code, String image) {
+        this.medicationId=medicationId;
         this.medicationName = medicationName;
         this.weight = weight;
         this.code = code;
